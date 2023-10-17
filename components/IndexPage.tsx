8dd89da31be860baa66104e8eff6e265bdefc2b0
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-17 13:25:18 +0300

    /* eslint-disable @next/next/no-img-element */

--- components/IndexPage.tsx
+++ components/IndexPage.tsx
@@ -7,6 +7,7 @@ import MoreStories from 'components/MoreStories'
 import IntroTemplate from 'intro-template'
 import * as demo from 'lib/demo.data'
 import type { Post, Settings } from 'lib/sanity.queries'
+import Footer from './footer'
 
 export interface IndexPageProps {
   preview?: boolean
@@ -39,6 +40,7 @@ export default function IndexPage(props: IndexPageProps) {
           )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
+        <Footer/>
       </Layout>
     </>
   )

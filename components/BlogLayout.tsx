ec97f3d1aabc213bac819ce2af5d79a3a8b02389
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-18 17:17:08 +0300

    import home into blog page

--- components/BlogLayout.tsx
+++ components/BlogLayout.tsx
@@ -1,5 +1,6 @@
 import AlertBanner from 'components/AlertBanner'
 import CustomNavbar from './layout/navbar'
+import Home from './layout/home-section'
 
 export default function BlogLayout({
   preview,
@@ -14,7 +15,7 @@ export default function BlogLayout({
     <>
       <div className="min-h-screen bg-blue-100">
         <div className="mb-8">
-          <CustomNavbar />
+          <Home/>
         </div>        <AlertBanner preview={preview} loading={loading} />
         <main>{children}</main>
       </div>

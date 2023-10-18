65780eee3c252a39c5edad67bb688e9674667236
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-18 10:58:26 +0300

    quick fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import HeroArea from '../components/home/hero-section'
-import Navbar from '../components/layout/navbar'
 import AboutSection from '../components/home/about-section'
 import ServiceSection from '../components/home/service-section'
 import MissionSection from '../components/home/misssion-section'

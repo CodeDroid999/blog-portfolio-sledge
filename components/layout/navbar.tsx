84466a8c8d6da503e08ec14631bb9273d1bda63e
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-18 13:15:01 +0300

    fix

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,8 +1,15 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CustomNavbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
   return (
     <div className="m-auto flex max-w-[1100px] items-center justify-between p-2 lg:p-3">
       {/**Mobile Nav */}
@@ -12,7 +19,7 @@ const CustomNavbar = () => {
           <Link href="/" className="text-gray-700">
             <div className="mb-1">
               <Image
-                src="/public/favicon/favicon.png"
+                src="https://i.postimg.cc/FRXRmLQ5/log.png"
                 alt="task"
                 width={130} // Set the width
                 height={60} // Set the height
@@ -22,17 +29,36 @@ const CustomNavbar = () => {
             </div>
           </Link>
         </div>
-
+     
+        {/* Mobile Menu (Dropdown) */}
+        {isMenuOpen && (
+          <div className="lg:hidden">
+            <div className="bg-white p-4 space-y-2">
+              <Link href="/" className="font-medium text-gray-700 hover:text-green-500">
+                HOME
+              </Link>
+              <Link href="/settings" className="font-medium text-gray-700 hover:text-green-500">
+                ABOUT
+              </Link>
+              {/* Add more links here */}
+            </div>
+          </div>
+        )}
         {/* Right div */}
         <div className="flex justify-end lg:hidden">
-          <div className="rounded-full p-3 text-white"></div>
+          <div className="rounded-full p-3 text-white">
+          <button onClick={toggleMenu} className="text-gray-700">
+            i minute
+          <FontAwesomeIcon icon="fa-solid fa-bars" style={{color: "#f2a807",}} />
+          </button>
+          </div>
         </div>
       </div>
 
       {/**Desktop */}
       <div className="hidden w-full items-center justify-between font-semibold lg:flex">
         <div className="flex flex-row items-center">
-          <div className="mr-10">
+          <div className="mr-1">
             <h1 className="text-4xl font-bold">
               <Link href="/" className="text-gray-700">
                 <div className="mb-1">

f7c4121b735dce3fb092ad10005512685771d974
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-18 16:43:29 +0300

    fix

--- components/layout/navMenu.tsx
+++ components/layout/navMenu.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
 import Link from 'next/link';
+import React from 'react';
 
 const NavigationMenu: React.FC = () => {
   return (
     <div className="bg-white w-100">
-      <ul className="navbar-nav">
-        <li className="nav-item active">
+      <ul className="navbar-nav p-1">
+        <li className="nav-item active p-2">
           <Link href="#home" passHref>
             <span className="nav-link text-left">Home <span className="sr-only">(current)</span></span>
           </Link>
         </li>
-        <li className="nav-item">
+        <li className="nav-item p-2">
           <Link href="#about" passHref>
             <span className="nav-link">About</span>
           </Link>
         </li>
-        <li className="nav-item">
+        <li className="nav-item p-2">
           <Link href="#services" passHref>
-            <span className="nav-link">Services</span>
+            <span className="nav-link pl-3">Services</span>
           </Link>
         </li>
-        <li className="nav-item">
+        <li className="nav-item p-2">
           <Link href="#projects" passHref>
-            <span className="nav-link">Projects</span>
+            <span className="nav-link pl-3">Projects</span>
           </Link>
         </li>
-        <li className="nav-item">
+        <li className="nav-item p-2">
           <Link href="#contact" passHref>
             <span className="nav-link">Contact us</span>
           </Link>

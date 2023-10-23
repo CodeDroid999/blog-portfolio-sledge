6dd19d931b8a02217aa0111a1c42d05d4dfae08d
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-23 12:26:04 +0300

    fix module error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
-import Layout from '../components/layout'
-import HeroArea from '../components/home/hero-section'
+
 import AboutSection from '../components/home/about-section'
-import ServiceSection from '../components/home/service-section'
+import HeroArea from '../components/home/hero-section'
 import MissionSection from '../components/home/misssion-section'
-import ReviewsSection from '../components/home/reviews-section'
-import VisionSection from '../components/home/vission-section'
 import ProjectsSection from '../components/home/projects-section'
+import ReviewsSection from '../components/home/reviews-section'
+import ServiceSection from '../components/home/service-section'
+import VisionSection from '../components/home/vission-section'
+import Layout from '../components/layout'
 import ContactForm from '../components/layout/contact-form'
 
 export default function Index() {

61a5ce014a08b9370d850dba2d2532deb75c4525
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-23 15:07:01 +0300

    add projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,20 +1,14 @@
+import ProjectsSection from 'components/home/projects-section'
 import Head from 'next/head'
 
-import AboutSection from '../components/home/about-section'
 import HeroArea from '../components/home/hero-section'
-import MissionSection from '../components/home/misssion-section'
-import ProjectsSection from '../components/home/projects-section'
-import ReviewsSection from '../components/home/reviews-section'
-import ServiceSection from '../components/home/service-section'
-import VisionSection from '../components/home/vission-section'
 import Layout from '../components/layout'
-import ContactForm from '../components/layout/contact-form'
 
-export default function Projects() {
+export default function Index() {
   return (
       <Layout>
+        <HeroArea />
         <ProjectsSection/>
-        <ContactForm />
       </Layout>
   )
 }

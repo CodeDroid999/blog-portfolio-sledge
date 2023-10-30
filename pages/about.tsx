e0ba69a957a7beaa4ca1ca371ed431e41066444a
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-30 11:14:08 +0300

    quick fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,11 +9,12 @@ import ServiceSection from '../components/home/service-section'
 import VisionSection from '../components/home/vission-section'
 import Layout from '../components/layout'
 import ContactForm from '../components/layout/contact-form'
+import AboutHeroArea from 'components/home/about-hero'
 
 export default function About() {
   return (
       <Layout>
-        <HeroArea />
+        <AboutHeroArea/>
         <AboutSection />
         <MissionSection/>
         <ReviewsSection/>
